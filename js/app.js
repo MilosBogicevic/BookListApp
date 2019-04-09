ba798801940a1d935e65a6a1dc8a873aa290b358
MilosBogicevic <milbog.development@gmail.com>
2019-04-09 09:30:34 +0200

    Added displayBooks option

--- js/app.js
+++ js/app.js
@@ -21,5 +21,37 @@ class UI {
             }
         ];
         const books = StoredBooks;
+
+        books.forEach((book) => UI.addBook(book));
+    }
+
+    static addBook() {
+        const list = document.querySelector("#book-list");
+        const row = document.createElement("tr");
+        
+        row.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
+        <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>`;
+        
+        list.appendChild(row);
     }
 }
+
+// Display Books
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
+
+// Add Books
+document.querySelector("#book-form").addEventListener("submit", (e) => {
+
+    e.preventDefault();
+    const title = document.querySelector("#title").value;
+    const author = document.querySelector("#author").value;
+    const isbn = document.querySelector("#isbn").value;
+
+    //Instatiate book
+    const book = new Book(title, author, isbn);
+
+    console.log(book)
+});
